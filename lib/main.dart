26de290dee7161011b0aa60762978a2b9aa6f02a
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-06-04 16:18:24 +0530

    used AppBar widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //Hello World
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
+      home: Scaffold(
+        //background color can also be added here
+        appBar: AppBar(
+          title: Text('I am Rich'), //Adding app bar title
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
       ),
     ),
   );
